179cbd824595959b110dcb510c3222eb2babd7c1
Dmitriy Efimov <dmitriy.efimov@yahoo.com>
2019-08-07 16:35:06 +0300

    feature(back): temp links external endpoint + service logic

--- src/Controller/ExternalController.php
+++ src/Controller/ExternalController.php
@@ -7,9 +7,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use \Symfony\Component\HttpKernel\Exception\HttpException;
 use App\Entity\Resource;
 use App\Service\ResourceService;
+use App\Service\LinkService;
 use App\Repository\ResourceRepository;
 use Symfony\Component\HttpFoundation\ResponseHeaderBag;
 
@@ -47,46 +49,67 @@ class ExternalController extends AbstractController
      * methods={"GET"},
      * name="resources_get_metadata")
      */
-     public function searchResources($_code, ResourceService $resourceService)
-     {
-       $response = new JsonResponse();
+    public function searchResources($_code, ResourceService $resourceService)
+    {
+      $response = new JsonResponse();
 
-       $metadata = $resourceService->getItemResourcesMetadata([$_code]);
-       if(!isset($metadata[$_code])||sizeof($metadata[$_code])<1){
-         throw new HttpException(404, "Не найдено ресурсов для товара");
-       }
-       $response->setData($metadata[$_code]);
-       return $response;
-     }
+      $metadata = $resourceService->getItemResourcesMetadata([$_code]);
+      if(!isset($metadata[$_code])||sizeof($metadata[$_code])<1){
+        throw new HttpException(404, "Не найдено ресурсов для товара");
+      }
+      $response->setData($metadata[$_code]);
+      return $response;
+    }
 
-     /**
-      * Получает по коду 1С и приоритету изображение ресурса, если указан jpg как формат, и нормализованный объект с информацией о ресурсе, если json
-      *
-      * @param string $_format Формат возвращаемого ресурса, подтягивется через wildcard {_format}
-      * @param string $_priority Приоритет изображения в группе, 1 соответсвует основному, 2 и далее соотвествуют приоритету дополнительного изображения -1
-      * @param string $_code Код 1С товара, подтягивается через wildcard {_code}
-      * @param ResourceService $resourceService Сервис для работы с ресурсами
-      *
-      * @Route(
-      *      "/getresource/{_code}_{_priority}_{_preset}.{_format}",
-      *      methods={"GET"},
-      *      name="catalogue_node_item_resource_by_priority_raw_optimized",
-      *      requirements={
-      *          "_code"="\d+",
-      *          "_priority"="\d{1,100}",
-      *          "_preset"="\d{1,3}",
-      *          "_format": "jpg|json|bool"
-      *      }
-      * )
-      */
-     public function getResourceByPriorityRaw($_format, $_priority, $_preset, $_code, ResourceRepository $resRepo)
-     {
-         $fields = $resRepo->getByItemPriorityPresetOptimized($_code, $_priority, $_preset);
-         $upload_directory = $this->getParameter('upload_directory');
-         $fullFilePath = $upload_directory .'/'. $fields['filepath'];
-         $src_filename = $fields['src_fn']?:'noName';
-         if(!file_exists($fullFilePath)){throw new HttpException(503, "Ресурс временно недоступен");}
-         return $this->file($fullFilePath, $src_filename, ResponseHeaderBag::DISPOSITION_INLINE);
-     }
+    /**
+     * Получает по коду 1С и приоритету изображение ресурса, если указан jpg как формат, и нормализованный объект с информацией о ресурсе, если json
+     *
+     * @param string $_format Формат возвращаемого ресурса, подтягивется через wildcard {_format}
+     * @param string $_priority Приоритет изображения в группе, 1 соответсвует основному, 2 и далее соотвествуют приоритету дополнительного изображения -1
+     * @param string $_code Код 1С товара, подтягивается через wildcard {_code}
+     * @param ResourceService $resourceService Сервис для работы с ресурсами
+     *
+     * @Route(
+     *      "/getresource/{_code}_{_priority}_{_preset}.{_format}",
+     *      methods={"GET"},
+     *      name="catalogue_node_item_resource_by_priority_raw_optimized",
+     *      requirements={
+     *          "_code"="\d+",
+     *          "_priority"="\d{1,100}",
+     *          "_preset"="\d{1,3}",
+     *          "_format": "jpg|json|bool"
+     *      }
+     * )
+     */
+    public function getResourceByPriorityRaw($_format, $_priority, $_preset, $_code, ResourceRepository $resRepo)
+    {
+        $fields = $resRepo->getByItemPriorityPresetOptimized($_code, $_priority, $_preset);
+        $upload_directory = $this->getParameter('upload_directory');
+        $fullFilePath = $upload_directory .'/'. $fields['filepath'];
+        $src_filename = $fields['src_fn']?:'noName';
+        if(!file_exists($fullFilePath)){throw new HttpException(503, "Ресурс временно недоступен");}
+        return $this->file($fullFilePath, $src_filename, ResponseHeaderBag::DISPOSITION_INLINE);
+    }
+
+    /**
+    * @Route("/api/links/gettemp/",
+    * methods={"POST"},
+    * name="resources_get_temp_links")
+    */
+    public function getTempLinks(Request $request, LinkService $linkService)
+    {
+      $data = json_decode(
+        $request->getContent(),
+        true
+      );
+      
+      $response = new JsonResponse();
+
+      $links = $linkService->getTempLinks($data);
+
+      $response->setData($links);
+
+      return $response;
+    }
 
 }
